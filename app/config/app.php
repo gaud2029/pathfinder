b37798d257bbb6b7f9566e39eeb3349c32414235
basderuiter <deruiterb@gmail.com>
2014-02-13 21:10:15 +0100

    base setup

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'http://localhost/pf',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -106,6 +106,12 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+
+        // laravel ide helper
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+
+        // Jeffrey Way's artisan generators
+        'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
